81d422d5824d66ca158658f930a3d20097abbe65
Anthony McIntosh <mcintant@uw.edu>
2026-03-05 08:24:09 -0500

    Updates

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'kobo-reader-v1';
+const urlsToCache = [
+  '.',
+  'index.html'
+];
+
+// Install event - cache resources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch event - serve from cache, fallback to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+
+
