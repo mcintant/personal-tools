11b97edbc6af7379e692a67b31d7598a174f9163
Anthony McIntosh <mcintant@uw.edu>
2026-03-06 08:46:19 -0500

    fix other

--- worker/.wrangler/tmp/bundle-yTepSt/middleware-insertion-facade.js
+++ worker/.wrangler/tmp/bundle-yTepSt/middleware-insertion-facade.js
@@ -1,10 +1,11 @@
 				import worker, * as OTHER_EXPORTS from "/Users/Tony/dev/kobo/worker/src/index.js";
 				import * as __MIDDLEWARE_0__ from "/Users/Tony/dev/kobo/worker/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "/Users/Tony/dev/kobo/worker/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
 
 				export * from "/Users/Tony/dev/kobo/worker/src/index.js";
 
 				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
 					
-					__MIDDLEWARE_0__.default
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
 				]
 				export default worker;
